aadcb0f9e0a399194c7736bd4fd7add58ab16955
moshaeMO <rarussell0903@gmail.com>
2025-04-23 16:36:44 -0500

    Basic Character Select

---
+++ Space-Shooter-Unity/Assets/ScriptableObjects/Characters/CharacterSO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class CharacterSO : ScriptableObject
+{
+    [Header("Character Information")]
+    public Image portrait;
+    public string _name;
+    public string bio;
+
+    [Header("Stats")]
+    public int health;
+    public float acceleration;
+    public float movementSpeed;
+    public float projectileSpeed;
+    public float fireRate;
+
+    [Header("Ship Prefab")]
+    public GameObject characterShip;
+}
