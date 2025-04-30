4cb3160a38d097666aadbc25c1b0a8831714601b
BigOnake <bigonake@gmail.com>
2025-04-30 14:50:12 -0500

    some changes lmao

--- Space-Shooter-Unity/Assets/ScriptableObjects/CurrentCharacterInfo.cs
+++ Space-Shooter-Unity/Assets/ScriptableObjects/CurrentCharacterInfo.cs
@@ -2,7 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CurrentCharacterInfo : MonoBehaviour
 {
-    public CharacterSO characterSO;
+    public List<CharacterSO> charactersList = new();
+    public CharacterSO curChar;
+
+    public void SelectCharacter(int i)
+    {
+        curChar = i > charactersList.Count ? charactersList[i] : null;
+    }
+
+    public CharacterSO GetCurChar()
+    {
+        return curChar;
+    }
 }
