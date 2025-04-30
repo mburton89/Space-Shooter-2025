a84e062ea1b93e4bf960a8b398d15e6551372b37
BigOnake <bigonake@gmail.com>
2025-04-30 16:11:14 -0500

    fixed transperency

--- Space-Shooter-Unity/Assets/Scripts/CharSelectUI.cs
+++ Space-Shooter-Unity/Assets/Scripts/CharSelectUI.cs
@@ -11,17 +11,28 @@ public class CharSelectUI : MonoBehaviour
     public TextMeshProUGUI l_name;
 
     public CurrentCharacterInfo currentCharacter;
+    private bool isSelected = false;
 
 
     private void Start()
     {
         currentCharacter = GetComponent<CurrentCharacterInfo>();
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0f);
     }
     public void UpdateUi()
     {
+        setSpriteVisibility();
         sprite.sprite = currentCharacter.curChar.portrait;
         bio.text = currentCharacter.curChar.bio;
         l_name.text = currentCharacter.curChar._name;
     }
 
+    private void setSpriteVisibility()
+    {
+        if (!isSelected)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 255f);
+            isSelected = true;
+        }
+    }   
 }
